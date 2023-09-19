e209146a23cb37d94d148e3a945fea480c209a71
Vijay Vithal Joshi <123143472+Vj-r12@users.noreply.github.com>
2023-09-20 02:24:29 +0530

    Add files via upload

---
+++ SQL Code File/Retail_Superstore_DDL_script.sql
@@ -0,0 +1,20 @@
+/*****     DDL SCRIPT     *****/
+
+
+-- Creating Table 
+
+CREATE TABLE SAMPLE_SUPERSTORE_INFO (
+									SHIP_MODE varchar,
+									SEGMENT varchar,
+									COUNTRY varchar,
+									CITY varchar,
+									STATE varchar,
+									POSTAL_CODE int,
+									REGION varchar,
+									CATEGORY varchar,
+									SUB_CATEGORY varchar,
+									AMOUNT_PER_UNIT float,
+									QUANTITY int,
+									DISCOUNT float,
+									PROFIT float
+									);
